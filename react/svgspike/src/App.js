393f1a72144c4314da0430929e84a6e3a24b419c
Blair Hone <honeb@shaw.ca>
2020-04-16 07:45:44 -0600

    refactoring

--- react/svgspike/src/App.js
+++ react/svgspike/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 // import logo from './logo.svg';
 import Rotate from './components/rotate.js';
 import Flip from './components/flip.js';
-import Cards from './components/cards.js';
+// import Controller from './components/controller.js';
+// import Cards from './components/cards.js';
 // import flip from './flip.svg';
 import om from './om.svg';
 import './App.css';
@@ -40,11 +41,20 @@ class App extends React.Component {
         mirrY: 0,
         cardColor: "green"
       },
-      cardTest: 1
     }
     this.rotateCard = this.rotateCard.bind(this)
   }
   rotateCard = (colorFromChild) => {
+    for (const a of Object.entries(this.state)) {
+      if (a[1].cardColor===colorFromChild) {
+        console.log("Found: ", a[1].cardColor)
+        
+        console.log(this.state.a);
+      }
+      // console.log(a[1].cardColor);
+      console.log(a[0]);
+      // console.log(a.search(colorFromChild)); 
+    }
     switch (colorFromChild) {
       case "black":
         // const rotation = this.state.blackCard.angle + 90;
@@ -57,21 +67,21 @@ class App extends React.Component {
         const redCard = this.state.redCard;
         redCard.angle = this.state.redCard.angle + 90;
         this.setState({"redCard":redCard});
-                console.log("red");
+        // console.log("red");
         break;
       case "khaki":
         // const rotation = this.state.khakiCard.angle + 90;
         const khakiCard = this.state.khakiCard;
         khakiCard.angle = this.state.khakiCard.angle + 90;
         this.setState({"khakiCard":khakiCard});
-                console.log("khaki");
+        // console.log("khaki");
         break;
       case "green":
         // const rotation = this.state.greenCard.angle + 90;
         const greenCard = this.state.greenCard;
         greenCard.angle = this.state.greenCard.angle + 90;
         this.setState({"greenCard":greenCard});
-        console.log("green");
+        // console.log("green");
         break;
       default:
         console.log("default")
@@ -100,10 +110,38 @@ class App extends React.Component {
             </div>
             <div className="symbolContainer">
               {/* <Cards card={om} angle={this.state.blackCard.angle}/> */}
-              <img id="omSymbol" src={om} alt="om symbol" className="om stack black" cardcolor="black" style={{transform: `rotate(${this.state.blackCard.angle}deg)`}} />
-              <img id="mSymbol" src={om} alt="om symbol" className="om stack red" cardcolor="red" style={{transform: `rotate(${this.state.redCard.angle}deg)`}}/>
-              <img id="Symbol" src={om} alt="om symbol" className="om stack darkkhaki"  cardcolor="khaki"style={{transform: `rotate(${this.state.khakiCard.angle}deg)`}}/>
-              <img id="ymbol" src={om} alt="om symbol" className="om forestgreen"  cardcolor="green"style={{transform: `rotate(${this.state.greenCard.angle}deg)`}}/>
+              <img
+                id="omSymbol"
+                src={om}
+                alt="om symbol"
+                className="om stack black"
+                cardcolor="black"
+                style={{transform: `rotate(${this.state.blackCard.angle}deg)`}}
+              />
+              <img
+                id="mSymbol"
+                src={om}
+                alt="om symbol"
+                className="om stack red"
+                cardcolor="red"
+                style={{transform: `rotate(${this.state.redCard.angle}deg)`}}
+              />
+              <img
+                id="Symbol"
+                src={om}
+                alt="om symbol"
+                className="om stack darkkhaki"
+                cardcolor="khaki"
+                style={{transform: `rotate(${this.state.khakiCard.angle}deg)`}}
+              />
+              <img
+                id="ymbol"
+                src={om}
+                alt="om symbol"
+                className="om forestgreen"
+                cardcolor="green"
+                style={{transform: `rotate(${this.state.greenCard.angle}deg)`}}
+              />
             </div>
             <div className="vertIcons">
               <Rotate filterColor="red" cardcolor="red" rotateThis={this.rotateCard}/>
